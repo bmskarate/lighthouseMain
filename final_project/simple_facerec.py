cb7490ebd8c5c5fd50a4836e45d5ea327dba31ee
Louis Rossi <bmskarate@gmail.com>
2021-11-24 14:21:52 -0500

    Final_project_nov241421

--- final_project/simple_facerec.py
+++ final_project/simple_facerec.py
@@ -8,8 +8,6 @@ class SimpleFacerec:
      def __init__(self):
          self.known_face_encodings = []
          self.known_face_names = []
-
-         # Resize frame for a faster speed
          self.frame_resizing = 0.25
 
      def load_encoding_images(self, images_path):
@@ -23,33 +21,34 @@ class SimpleFacerec:
 
          print("{} encoding images found.".format(len(images_path)))
 
-         # Store image encoding and names
+         # Store img and names
          for img_path in images_path:
              img = cv2.imread(img_path)
              rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-             # Get the filename only from the initial file path.
-             basename = os.path.basename(img_path)
+             basename = os.path.basename(img_path) # filename from original path
              (filename, ext) = os.path.splitext(basename)
-             # Get encoding
-             img_encoding = face_recognition.face_encodings(rgb_img)[0]
-
-             # Store file name and file encoding
-             self.known_face_encodings.append(img_encoding)
+             img_encoding = face_recognition.face_encodings(rgb_img)[0] # get encoding
+             self.known_face_encodings.append(img_encoding) # store
              self.known_face_names.append(filename)
-         print("Encoding images loaded")
+         print("Images encoded and loaded.")
 
      def detect_known_faces(self, frame):
+         """
+         Find faces.
+         Convert to BGR (opencv) to RGB (face_recognition)
+         """
          small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
-         # Find all the faces and face encodings in the current frame of video
-         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
          rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
          face_locations = face_recognition.face_locations(rgb_small_frame)
          face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
+         """
+         for loop:
+         Check for match, if match, return name. If not, 'Unknown'
+         """
          face_names = []
          for face_encoding in face_encodings:
-             # See if the face is a match for the known face(s)
+             
              matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
              name = "Unknown"
 
@@ -65,7 +64,6 @@ class SimpleFacerec:
                  name = self.known_face_names[best_match_index]
              face_names.append(name)
 
-         # Convert to numpy array to adjust coordinates with frame resizing quickly
-         face_locations = np.array(face_locations)
+         face_locations = np.array(face_locations) # adjust coordinates, numpy array, fast resizing
          face_locations = face_locations / self.frame_resizing
          return face_locations.astype(int), face_names
